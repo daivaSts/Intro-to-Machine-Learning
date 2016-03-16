607d14bcd059e143a110b869c61270a4fec63e70
daivaSts <daiva.satas@gmail.com>
2016-03-16 08:47:11 -0500

    create and train a Naive Bayes classifier

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -16,18 +16,44 @@ sys.path.append("../tools/")
 from email_preprocess import preprocess
 
 
-### features_train and features_test are the features for the training
-### and testing datasets, respectively
-### labels_train and labels_test are the corresponding item labels
+### features_train are features for the training
+### features_test are features for testing datasets
+### labels_train are the train labels 
+### labels_test are the test labels
+
 features_train, features_test, labels_train, labels_test = preprocess()
 
+def NBAccuracy(features_train, labels_train, features_test, labels_test):
+    """ compute the accuracy of your Naive Bayes classifier """
+    ### import the sklearn module for GaussianNB
+    from sklearn.naive_bayes import GaussianNB
+    from sklearn.metrics import accuracy_score
+
+    ### create classifier
+    clf = GaussianNB()
+
+    ### fit the classifier on the training features and labels. 
+    t0 = time()
+    fit_classifier = clf.fit(features_train, labels_train)
+    print "training time:", round(time()-t0, 3), "s"
+
+    ### use the trained classifier to predict labels for the test features
+    t0 = time()
+    pred = clf.predict(features_test)
+    print "predict time:", round(time()-t0, 3), "s"
+
+    ### calculate and return the accuracy on the test data
+
+    ## V1
+    #accuracy = clf.score(features_test, labels_test)
+
+    ## V2
+    accuracy = accuracy_score(labels_test, pred)
+
+    return accuracy
 
 
+NBAccuracy(features_train, labels_train, features_test, labels_test)
 
-#########################################################
-### your code goes here ###
-
-
-#########################################################
 
 
